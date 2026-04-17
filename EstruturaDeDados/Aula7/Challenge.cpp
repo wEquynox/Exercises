da4a43c4f257a5634ebd7c56d3f114e100043109
Noxy <gabrielhemannboff@gmail.com>
2026-04-16 21:38:10 -0300

    adjusting activities

--- EstruturaDeDados/Aula7/Challenge.cpp
+++ EstruturaDeDados/Aula7/Challenge.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 
+//Recebe dois endereços, coloca os endereços nas duas variáveis pont e realiza a troca de valores 
+void switchNumbersWPont(int *pont1, int *pont2) {
+    int temp = *pont1;
+    *pont1 = *pont2;
+    *pont2 = temp;
+}
+//Recebe duas variáveis que viram referencia para os dois pont e por fim, realiza a troca de posições
+void switchNumbersWRef(int &pont1, int &pont2) {
+    int temp = pont1;
+    pont1 = pont2;
+    pont2 = temp;
+}
+
 int main() {
 
-    int numberOne, numberTwo;
+    int numberOne, numberTwo; //Cria duas variáveis comuns
 
+    //Lê duas variáveis do usuário
     std::cout << "Type two numbers: " << std::endl;
     std::cin >> numberOne;
     std::cin >> numberTwo;
 
-    int *adress1 = &numberOne;
-    int *adress2 = &numberTwo;
+    switchNumbersWPont(&numberOne, &numberTwo); //Chama a função de ponteiros
 
-    int temp = *adress1;
-    *adress1 = *adress2;
-    *adress2 = temp;
+    std::cout << "The switched numbers are: " << numberOne << " and " << numberTwo << std::endl;
+
+    switchNumbersWRef(numberOne, numberTwo); //Chama a função de referências
 
     std::cout << "The switched numbers are: " << numberOne << " and " << numberTwo << std::endl;
 
